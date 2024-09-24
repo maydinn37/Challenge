1d4f7558f729be6ea53000f7764153a39e79b0d1
maydinn37 <mertaydinn37@gmail.com>
2024-09-24 10:59:51 +0300

    update

---
+++ 5.5/script.js
@@ -0,0 +1,17 @@
+let number = 0;
+let board = document.querySelector(".board");
+
+function plus() {
+  number += 1;
+  board.innerHTML = number;
+}
+
+function reset() {
+  number = 0;
+  board.innerHTML = number;
+}
+
+function negative() {
+  number -= 1;
+  board.innerHTML = number;
+}
