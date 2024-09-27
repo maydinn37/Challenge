12a2ee1a19aa0cbe6ad2c4f77f6cde9752cb854e
maydinn37 <mertaydinn37@gmail.com>
2024-09-27 12:26:57 +0300

    update

--- 6/app.js
+++
@@ -1,9 +0,0 @@
-let accordion = document.querySelectorAll(".accordion");
-accordion.forEach((acc) => {
-  acc.onclick = () => {
-    accordion.forEach((close) => {
-      close.classList.remove("active");
-    });
-    acc.classList.add("active");
-  };
-});
