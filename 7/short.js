009d50e12b90fe1536817f6976b28d6dc1661e02
maydinn37 <mertaydinn37@gmail.com>
2024-10-08 08:57:59 +0300

    update

--- 7/short.js
+++ 7/short.js
@@ -1,4 +1,4 @@
-let btnAll = document.querySelector(".btn-all");
+/*let btnAll = document.querySelector(".btn-all");
 let btnPhoto = document.querySelector(".btn-photo");
 let btnGame = document.querySelector(".btn-game");
 let imgPhoto = document.querySelectorAll(".photo");
@@ -37,6 +37,39 @@ btn.forEach(function (btnItem) {
       imgPhoto.forEach(function (p) {
         p.classList.add("show");
       });
+    }
+  });
+});
+*/
+const btnAll = document.querySelector(".btn-all");
+const btnPhoto = document.querySelector(".btn-photo");
+const btnGame = document.querySelector(".btn-game");
+const btn = document.querySelectorAll(".btn");
+const images = document.querySelectorAll(".images");
+
+btn.forEach((item, index) => {
+  btn[index].addEventListener("click", function () {
+    btn[index].classList.toggle("active");
+    const btnImageClass = btn[index].classList[1].split("-")[1];
+    console.log(btn);
+    images.forEach((item, index) => {
+      const imageClass = item.classList[1];
+
+      if (btnImageClass != imageClass) {
+        item.classList.toggle("nonShow");
+      } else {
+        item.classList.remove("nonShow");
+      }
+
+      if (btnImageClass == "all") {
+        item.classList.remove("nonShow");
+      }
+    });
+
+    for (var i = 0; i < btn.length; i++) {
+      if (index != i) {
+        btn[i].classList.remove("active");
+      }
     }
   });
 });
