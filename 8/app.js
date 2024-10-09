652bd43a75c9bf68e1468b88d831c7c898ab0417
maydinn37 <mertaydinn37@gmail.com>
2024-10-09 14:08:30 +0300

    uptade

--- 8/app.js
+++ 8/app.js
@@ -6,27 +6,36 @@
 //   });
 // });
 const cards = document.querySelectorAll("img");
-const cvv = document.querySelector(".cvv-input");
-const namee = document.querySelector(".namee");
-const cardDate = document.querySelector(".date");
-const number = document.querySelector(".number");
-const cardNamePre = document.querySelector(".card-name-pre");
-const cardNumPre = document.querySelector(".card-number-pre");
-const cardDatePre = document.querySelector(".card-date-pre");
+const nameInput = document.querySelector(".name-input");
 const cvvPre = document.querySelector(".cvv-pre");
 const form = document.querySelectorAll("form input");
+const images = document.querySelectorAll(".images p");
+form.forEach((item, index) => {
+  form[index].addEventListener("input", function () {
+    images.forEach((item, index) => {
+      images[index].textContent = form[index].value;
+    });
+  });
+});
 
 form.forEach((item, index) => {
   form[index].addEventListener("click", function () {
     cards.forEach((card) => {
       card.style.animation = "start 2s forwards";
     });
-    cvvPre.classList.remove("active");
+    cvvPre.classList.remove("active-cvv");
+    images.forEach((item, index) => {
+      images[index].classList.add("active");
+      images[3].classList.remove("active");
+    });
   });
 });
 form[3].addEventListener("click", function () {
   cards.forEach((card) => {
     card.style.animation = "spin 2s forwards";
   });
-  cvvPre.classList.add("active");
+  cvvPre.classList.add("active-cvv");
+  images.forEach((item, index) => {
+    images[index].classList.remove("active");
+  });
 });
