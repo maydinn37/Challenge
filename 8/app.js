5a924e1feb36fae8cd572e404efb6b4554db199b
maydinn37 <mertaydinn37@gmail.com>
2024-10-09 12:36:28 +0300

    update

--- 8/app.js
+++ 8/app.js
@@ -7,27 +7,26 @@
 // });
 const cards = document.querySelectorAll("img");
 const cvv = document.querySelector(".cvv-input");
-const name = document.querySelector(".name");
+const namee = document.querySelector(".namee");
 const cardDate = document.querySelector(".date");
 const number = document.querySelector(".number");
-cvv.addEventListener("click", function () {
+const cardNamePre = document.querySelector(".card-name-pre");
+const cardNumPre = document.querySelector(".card-number-pre");
+const cardDatePre = document.querySelector(".card-date-pre");
+const cvvPre = document.querySelector(".cvv-pre");
+const form = document.querySelectorAll("form input");
+
+form.forEach((item, index) => {
+  form[index].addEventListener("click", function () {
+    cards.forEach((card) => {
+      card.style.animation = "start 2s forwards";
+    });
+    cvvPre.classList.remove("active");
+  });
+});
+form[3].addEventListener("click", function () {
   cards.forEach((card) => {
     card.style.animation = "spin 2s forwards";
   });
-});
-
-name.addEventListener("click", function () {
-  cards.forEach((card) => {
-    card.style.animation = "start 2s forwards";
-  });
-});
-number.addEventListener("click", function () {
-  cards.forEach((card) => {
-    card.style.animation = "start 2s forwards";
-  });
-});
-cardDate.addEventListener("click", function () {
-  cards.forEach((card) => {
-    card.style.animation = "start 2s forwards";
-  });
+  cvvPre.classList.add("active");
 });
