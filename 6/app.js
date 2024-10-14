fd1e440d43fe7503eef7b55f5383b65f443f4048
maydinn37 <mertaydinn37@gmail.com>
2024-10-14 12:44:05 +0300

    update

--- 6/app.js
+++ 6/app.js
@@ -1,15 +1,23 @@
 let accordionCont = document.querySelectorAll(".accordion_content");
 let accordion = document.querySelectorAll(".accordion");
 let accordionHed = document.querySelectorAll(".accordion_header");
-
 accordion.forEach(function (item) {
   item.addEventListener("click", function () {
     accordionCont.forEach(function (contItem) {
       contItem.classList.remove("active");
     });
+    accordionHed.forEach(function (item) {
+      item.classList.remove("rotate");
+    });
     let content = this.querySelector(".accordion_content");
+    let accordionHed2 = this.querySelectorAll(".accordion_header");
     if (content) {
       content.classList.toggle("active");
+    }
+    if (accordionHed2) {
+      accordionHed2.forEach(function (item) {
+        item.classList.add("rotate");
+      });
     }
   });
 });
