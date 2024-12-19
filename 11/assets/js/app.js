a2f1d1f5791610776653abcb69a846b3337c6eaf
maydinn37 <mertaydinn37@gmail.com>
2024-12-19 10:03:52 +0300

    update

--- 11/assets/js/app.js
+++ 11/assets/js/app.js
@@ -7,3 +7,68 @@ $("#hamburger").click(function () {
     $(".buttons").fadeToggle();
   }
 });
+
+$(document).ready(function () {
+  var owl = $("#best-vacation-carousel").owlCarousel({
+    rewind: false,
+    loop: true,
+    stagePadding: 0,
+    nav: false,
+    items: 3,
+    margin: 31,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      800: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+
+  $("#prevButton").click(function () {
+    owl.trigger("prev.owl.carousel");
+  });
+
+  $("#nextButton").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+});
+
+$(document).ready(function () {
+  var owl = $("#book-now-carousel").owlCarousel({
+    rewind: false,
+    loop: true,
+    items: 4,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      800: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+
+  $("#prevButtonBook").click(function () {
+    owl.trigger("prev.owl.carousel");
+  });
+
+  $("#nextButtonBook").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+});
